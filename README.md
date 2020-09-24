e1451c4f09974c64cc7693e4c0e5e9ec0dae4fb1
xiong-xiong <juha.stenroos@frantic.com>
2020-09-24 19:50:39 +0300

    setup version 2

---
+++ README.md
@@ -0,0 +1,37 @@
+# compound interest calculator
+
+Compound interest calculator with Vue.js
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Run your tests
+
+```
+npm run test
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>Subway Calorie counter</h1>
+    <div class="bg" />
+    <div class="calculator">
+      <Calculator
+        v-bind:breads="breads"
+        v-bind:sauces="sauces"
+        v-bind:meats="meats"
+        v-bind:cheeses="cheeses"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+import { breads, cheeses, meats, sauces } from "@/data/data.js";
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+  data() {
+    return {
+      breads: breads,
+      cheeses: cheeses,
+      meats: meats,
+      sauces: sauces,
+    };
+  },
+};
+</script>
+
+<style>
+@import "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css";
+h1 {
+  color: yellow;
+  -webkit-text-stroke: 1px black;
+  text-transform: uppercase;
+}
+.home {
+  position: relative;
+}
+.bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: -2;
+  opacity: 0.06;
+}
+</style>
